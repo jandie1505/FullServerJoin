ca0e73e723b2bf471689705007b5ba84923e94be
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2024-12-25 17:54:11 +0100

    switched build system to gradle and api to paper api

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+}
+
+java {}
